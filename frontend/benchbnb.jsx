bed453b663ade2c4a2110dd510040675401e82ed
agarun <19801205+agarun@users.noreply.github.com>
2017-12-22 23:18:25 -0500

    Add top-level session and errors reducers

--- frontend/benchbnb.jsx
+++ frontend/benchbnb.jsx
@@ -7,6 +7,13 @@ window.login = A.login;
 window.signup = A.signup;
 window.signout = A.signout;
 
+import configureStore from './store/store';
+
+const store = configureStore();
+
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Benchbnb</h1>, root);
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const initialState = {
+  currentUser: null
+};
+
+const sessionReducer = (state = initialState, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return action.currentUser;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
