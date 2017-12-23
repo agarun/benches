294914726398449136c7c9a2a13c18b18e345062
agarun <19801205+agarun@users.noreply.github.com>
2017-12-23 13:55:43 -0500

    Fix user actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -16,25 +16,29 @@ export const receiveErrors = errors => ({
 export const login = user => dispatch => (
   SessionApiUtil
     .login(user)
-    .then(payload => {
-      dispatch(receiveCurrentUser(payload));
-      return payload;
-    })
+    .then(payload => (
+      dispatch(receiveCurrentUser(payload))
+    ), error => (
+      dispatch(receiveErrors(error.responseJSON))
+    ))
 );
 
 export const logout = () => dispatch => (
   SessionApiUtil
     .signout()
-    .then(() => {
-      dispatch(receiveCurrentUser(null));
-    })
+    .then(() => (
+      dispatch(receiveCurrentUser(null))
+    ), error => (
+      dispatch(receiveErrors(error.responseJSON))
+    ))
 );
 
 export const signup = user => dispatch => (
   SessionApiUtil
     .signup(user)
-    .then(payload => {
-      dispatch(receiveCurrentUser(payload));
-      return payload;
-    })
+    .then(payload => (
+      dispatch(receiveCurrentUser(payload))
+    ), error => (
+      dispatch(receiveErrors(error.responseJSON))
+    ))
 );
