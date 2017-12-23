8ccdaee139ea9f870806bf6109c0503f84728fe9
agarun <19801205+agarun@users.noreply.github.com>
2017-12-23 15:44:13 -0500

    Add bench model and controller

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,7 @@
+class Api::BenchesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'benches/index'
+
+  get 'benches/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create]
     resource :session, only: [:create, :destroy]
+    resources :benches, only: [:index, :create]
   end
 end
